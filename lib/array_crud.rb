2f5bfc77f300dbb2ee88ad09eea54da14b06454b
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-07 22:22:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,9 +6,10 @@ def create_an_array
   ["blackberry", "cranberry", "hawthorne", "juniper"]
 end
 
-def add_element_to_end_of_array(array, element)
+ def 
  array = ["wow", "I", "am", "really", "learning"]
-   add_element_to_end_of_array("array.push","arrays!")
+  array.push("arrays!")
+  p array
 end
 
 def add_element_to_start_of_array(array, element)
