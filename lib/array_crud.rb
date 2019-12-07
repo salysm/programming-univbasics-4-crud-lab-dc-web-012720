16e648ef474641f0e8a4ee2dd8717f0d6c1b1d32
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-07 22:17:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
  berries = ["blackberry", "cranberry", "hawthorne", "juniper"]
-  add_element_to_end_of_array.push(berries("blueberry"))
+  berries.push("blueberry")
 end
 
 def add_element_to_start_of_array(array, element)
